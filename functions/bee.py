1e0b48262b151d7221610cce624003969ce52c78
notpatrickadams <32269304+notpatrickadams@users.noreply.github.com>
2022-03-21 11:35:45 -0400

    Added Pokemon command, moved functions into sub directory

---
+++ functions/bee.py
@@ -0,0 +1,43 @@
+# Creates images of bees by replacing the following values:
+# (237, 28, 36)
+# (255, 242, 0)
+# (63, 72, 204)
+
+from io import BytesIO
+from random import randint
+from PIL import Image
+import os
+
+def generate_random():
+    with Image.open("bee.png") as img:
+        a = []
+        data = img.getdata()
+
+        rand1 = (randint(0, 255), randint(0, 255), randint(0, 255), 255)
+        rand2 = (randint(0, 255), randint(0, 255), randint(0, 255), 255)
+        rand3 = (randint(0, 255), randint(0, 255), randint(0, 255), 255)
+
+        filename = str(hash(rand1 + rand2 + rand3)) + ".png"
+
+        # In case somehow a bee was already generated
+        if os.path.exists(filename):
+            return filename
+
+        for item in data:
+            if item == (237, 28, 36, 255):
+                a.append(rand1)
+            elif item == (255, 242, 0, 255):
+                a.append(rand2)
+            elif item == (63, 72, 204, 255):
+                a.append(rand3)
+            else:
+                a.append(item)
+
+        img.putdata(a)
+        with BytesIO() as image_binary:
+            img.save(image_binary, "PNG")
+            image_binary.seek(0)
+            with open("temp.png", "wb") as t:
+                t.write(image_binary.read())
+
+            return "temp.png"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord, os, logging
-from bee import generate_random
 from dotenv import load_dotenv
+
+from functions.bee import generate_random
+from functions.pokemon import get_pokemon
 
 client = discord.Client()
 load_dotenv()
@@ -17,12 +19,21 @@ async def on_message(message):
     elif message.content.startswith("!bee"):
         logging.info(f"Bee requested by { message.author.mention }")
         filename = generate_random()
-        with open(filename, 'rb') as f:
-            file_to_send = discord.File(f, filename=filename)
-        await message.channel.send("Here's your bee!", file=file_to_send)
+        await message.channel.send("Here's your bee!", file=discord.File(fp=filename, filename="image.png"))
         logging.info("Bee successfully generated and sent")
     elif "lamp" in message.content.lower():
         logging.info(f"Sent moth image to { message.author.mention }")
         await message.channel.send("Moth summoned.", file=discord.File(fp="moth.jpg"))
+    elif message.content.startswith("!pokemon"):
+        logging.info(f"Pokemon requested by { message.author.mention }")
+        pkmn = get_pokemon()
+        emb = discord.Embed(title=pkmn["name"], description=pkmn["genus"], thumbnail=pkmn["icon"], color=0xDDDDDD)
+        emb.set_image(url=pkmn["icon"])
+        emb.add_field(name="Dex Number", value=pkmn["dex_no"], inline=False)
+        emb.add_field(name="Flavor text", value=pkmn["flavor_text"], inline=False)
+        emb.add_field(name="Types", value=", ".join(pkmn["types"]), inline=False)
+        await message.channel.send(embed=emb)
+        logging.info(f"Pokemon: {pkmn['name']} #{pkmn['dex_no']} sent")
+
 
 client.run(os.getenv("DISCORD_TOKEN"))
